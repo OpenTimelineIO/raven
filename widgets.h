e6f5ca642b89ebf424cbed5b6025f5d06c4e60ac
Joshua Minor <jminor@users.noreply.github.com>
2022-11-24 15:09:27 -0800

    Removed left over Knob declarations.

--- widgets.h
+++ widgets.h
@@ -10,28 +10,3 @@ bool Splitter(
     float min_size2,
     float splitter_long_axis_size = -1.0f,
     float hover_extend = 0.0f);
-bool KnobScalar(
-    const char* label,
-    ImGuiDataType data_type,
-    void* p_data,
-    float v_speed,
-    const void* p_min,
-    const void* p_max,
-    const char* format,
-    ImGuiSliderFlags flags);
-bool KnobFloat(
-    const char* label,
-    float* v,
-    float v_speed = 1.0f,
-    float v_min = 0.0f,
-    float v_max = 1.0f,
-    const char* format = "%.2f",
-    ImGuiSliderFlags flags = 0);
-bool KnobInt(
-    const char* label,
-    int* v,
-    float v_speed = 1.0f,
-    int v_min = 0.0f,
-    int v_max = 1.0f,
-    const char* format = "%d",
-    ImGuiSliderFlags flags = 0);
