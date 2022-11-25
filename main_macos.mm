7f9ddf37fd1adc63a7223db4810a9929e2307e1f
Joshua Minor <jminor@users.noreply.github.com>
2022-11-24 21:01:24 -0800

    Remove leftover unused variables.

--- main_macos.mm
+++ main_macos.mm
@@ -98,8 +98,6 @@ int main(int argc, char** argv)
     MTLRenderPassDescriptor *renderPassDescriptor = [MTLRenderPassDescriptor new];
 
     // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
     float clear_color[4] = {0.45f, 0.55f, 0.60f, 1.00f};
 
     // Main loop
