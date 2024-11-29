12ebf08575cedb4fda62301d073c1014644bad2e
Thomas Wilshaw <thomaswilshaw@gmail.com>
2024-11-29 16:55:13 +0000

    Timeline arrow key navigation
    Add inital support for left and right arrow key navigation in the
    timeline. It was necessary to turn off the default navigation as it was
    interacting badly.
    
    Signed-off-by: Thomas Wilshaw <thomaswilshaw@gmail.com>

--- main_win32.cpp
+++ main_win32.cpp
@@ -87,7 +87,7 @@ int main(int argc, char** argv)
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
