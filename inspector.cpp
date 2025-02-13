ccb40b8f03c18daa625f7340d4c47e36ff164933
Joshua Minor <joshm@pixar.com>
2025-02-13 00:31:18 -0800

    Hide the Start Time column by default
    Signed-off-by: Joshua Minor <joshm@pixar.com>

--- inspector.cpp
+++ inspector.cpp
@@ -888,7 +888,7 @@ void DrawTreeInspector() {
                           ImGuiTableFlags_Hideable)) {
         ImGui::TableSetupColumn("Name", ImGuiTableColumnFlags_WidthStretch);
         ImGui::TableSetupColumn("Type", ImGuiTableColumnFlags_WidthFixed);
-        ImGui::TableSetupColumn("Start Time", ImGuiTableColumnFlags_WidthFixed);
+        ImGui::TableSetupColumn("Start Time", ImGuiTableColumnFlags_WidthFixed | ImGuiTableColumnFlags_DefaultHide);
         ImGui::TableSetupColumn("Global Start Time", ImGuiTableColumnFlags_WidthFixed);
         ImGui::TableSetupColumn("Duration", ImGuiTableColumnFlags_WidthFixed);
 
