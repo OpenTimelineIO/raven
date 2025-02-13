12e1d8027079f9bedd392ef4c0274851f7cb7a28
Joshua Minor <joshm@pixar.com>
2025-02-13 00:28:47 -0800

    Remove the Clips inspector (replaced by tree)
    Signed-off-by: Joshua Minor <joshm@pixar.com>

--- app.cpp
+++ app.cpp
@@ -627,7 +627,6 @@ void MainGui() {
         ImGui::DockBuilderDockWindow("Timeline", dock_main_id);
         ImGui::DockBuilderDockWindow("Inspector", dock_id_side);
         ImGui::DockBuilderDockWindow("JSON", dock_id_side);
-        ImGui::DockBuilderDockWindow("Clips", dock_id_side);
         ImGui::DockBuilderDockWindow("Tree", dock_id_side);
         ImGui::DockBuilderDockWindow("Markers", dock_id_side);
         ImGui::DockBuilderDockWindow("Effects", dock_id_side);
@@ -679,13 +678,6 @@ void MainGui() {
     visible = ImGui::Begin("JSON", NULL, window_flags);
     if (visible) {
         DrawJSONInspector();
-    }
-    ImGui::End();
-
-    ImGui::SetNextWindowDockID(dockspace_id, ImGuiCond_FirstUseEver);
-    visible = ImGui::Begin("Clips", NULL, window_flags);
-    if (visible) {
-        DrawClipsInspector();
     }
     ImGui::End();
 
